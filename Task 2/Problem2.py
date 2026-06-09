b32164f55aad9a110799e0496301fde4a9a91720
SakiraKhan0608 <sakirakhan.0608@gmail.com>
2026-06-09 15:58:09 +0600

    Update Problem2.py

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,17 @@
 
+text=input().strip()
+k=int(input())
+
+count={}
+
+for i in range(len(text)-k+1):
+  pattern=text[i:i+k]
+  count[pattern]=count.get(pattern,0)+1
+
+max_count=max(count.values())
+result=[]
+for p in count:
+  if count[p]==max_count:
+    result.append(p)
+
+print(*result)
