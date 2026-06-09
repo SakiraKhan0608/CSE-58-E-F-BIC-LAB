1d25c8e6d39a3cac63afaa9ab35a75e1d636ae55
SakiraKhan0608 <sakirakhan.0608@gmail.com>
2026-06-09 16:01:16 +0600

    Update Problem4.py

--- Task 2/Problem4.py
+++ Task 2/Problem4.py
@@ -1,1 +1,11 @@
 
+pattern=input().strip()
+genome=input().strip()
+
+pos=[]
+
+for i in range (len(genome)-len(pattern)+1):
+  if genome[i:i+len(pattern)]==pattern:
+    pos.append(str(i))
+
+print(" ".join(pos))
