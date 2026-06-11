22fc7cd04ad92d5427e5c08dafa230212e0ec426
SakiraKhan0608 <sakirakhan.0608@gmail.com>
2026-06-11 16:35:10 +0600

    Update Problem3.py

--- Task 2/Problem3.py
+++ Task 2/Problem3.py
@@ -1,8 +1,26 @@
-pattern = input().strip()
-comp={'A':'T','T':'A','C':'G','G':'C'}
-result=""
+# pattern = input().strip()
+# comp={'A':'T','T':'A','C':'G','G':'C'}
+# result=""
 
-for c in reversed(pattern):
-  result+=comp[c]
+# for c in reversed(pattern):
+#   result+=comp[c]
 
-print(result)
+# print(result)
+
+
+
+string=input()
+
+s=""
+for char in string:
+   if char=='A':
+    s+='T'
+   elif char=='T':
+    s+='A'
+   elif char=='G':
+    s+='C'
+   elif char=='C':
+    s+='G'
+
+rev=s[::-1]
+print(rev)
