b9685a1eaab2ae809d602e745808db381c7bbaa8
SakiraKhan0608 <sakirakhan.0608@gmail.com>
2026-06-16 16:00:35 +0600

    Update Problem2.py

--- Task 3/Problem2.py
+++ Task 3/Problem2.py
@@ -1,1 +1,13 @@
+pattern=input()
+text=input()
+d=int(input())
 
+for i in range(len(text)-len(pattern)+1):
+  substr=text[i:i+len(pattern)]
+  mismatch=0
+  for j in range(len(pattern)):
+    if pattern[j]!=substr[j]:
+      mismatch+=1
+
+  if mismatch<=d:
+    print(i)
